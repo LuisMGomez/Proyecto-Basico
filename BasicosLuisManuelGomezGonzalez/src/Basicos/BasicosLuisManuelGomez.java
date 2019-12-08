10990b647fbbd3895f4a6e27c7c343f9c2e0b44a
Luis Manuel Gomez <gomez.luismnl@gmail.com>
2019-12-08 22:51:09 +0100

    Proyecto Basico Inicial

---
+++ BasicosLuisManuelGomezGonzalez/src/Basicos/BasicosLuisManuelGomez.java
@@ -0,0 +1,16 @@
+package Basicos;
+
+public class BasicosLuisManuelGomez {
+	
+	/* Tarea : Proyecto Basico
+	 * Entornos de Desarrollo
+	 * 
+	 * @author Luis Manuel Gomez Gonzalez
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
