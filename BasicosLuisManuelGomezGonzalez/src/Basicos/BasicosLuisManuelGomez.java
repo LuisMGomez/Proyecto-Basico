480ef71003287d2b14f9037a77efe276348a66e9
Luis Manuel Gomez <gomez.luismnl@gmail.com>
2019-12-09 00:30:32 +0100

    Creaccion Metodo Logico: definición, asignación y visualización

--- BasicosLuisManuelGomezGonzalez/src/Basicos/BasicosLuisManuelGomez.java
+++ BasicosLuisManuelGomezGonzalez/src/Basicos/BasicosLuisManuelGomez.java
@@ -10,7 +10,31 @@ public class BasicosLuisManuelGomez {
 
 	public static void main(String[] args) {
 		
-
+		// Ejemplo tipo logicos
+		logicos();
 	}
+	
+	/**
+	 * Ejemplos de tipo Logicos
+	 */
 
+	public static void logicos() {
+
+		// Declaracion de tipo Boolean
+
+		Boolean on;
+		Boolean off;
+
+		// Asignacion de valores de tipos Boolean
+
+		on = true;
+		off = false;
+
+		// Mostrar en pantalla el contendido de tipos Boolean
+
+		System.out.println("\nEjemplos de tipo Boolean:");
+		System.out.println("-----------------------------------");
+		System.out.println("Valor on.....: " + on);
+		System.out.println("Valor off....: " + off);
+	}
 }
